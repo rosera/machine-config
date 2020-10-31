5c19a5b96f5d1a761702075538a87959b908c91b
rosera <askrichardrose@gmail.com>
2020-10-31 21:13:57 +0000

    Remove old directory

--- tmux/tmux-scripts/uptime.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-uptime | awk -F'( |,|:)+' '{print $6,"hours,", $7,"minutes",$8,$9}'
--- tmux/tmux-scripts/whatsmyip.sh
+++
@@ -1,5 +0,0 @@
-P_ADDR=$(curl -s http://myip.enix.org/REMOTE_ADDR)
-[ "$MY_IP_ADDR" ] || {
-  echo "?.?.?.?"
-  exit 1
-}
