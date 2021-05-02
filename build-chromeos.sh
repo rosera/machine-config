b78face6cf5b4ae6799854e0e148e623d8e164db
rosie <askrichardrose@gmail.com>
2021-05-02 14:37:32 +0100

    Update build-chromeos.sh
    Add progress bar to curl cmd

--- build-chromeos.sh
+++ build-chromeos.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Download chromeos.zip
-#curl -Lo chromeos.zip https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_13729.56.0_guado_recovery_stable-channel_mp-v2.bin.zip
+# Download chromeos.zip R87
+#curl -# -Lo chromeos.zip https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_13729.56.0_guado_recovery_stable-channel_mp-v2.bin.zip
 # R87
-curl -Lo chromeos.zip https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_13505.73.0_guado_recovery_stable-channel_mp-v2.bin.zip
-# Download Brunch release
-#curl -Lo brunch.tar.gz https://github.com/sebanc/brunch/releases/download/r89-stable-20210403/brunch_r89_stable_20210403.tar.gz
-curl -Lo brunch.tar.gz https://github.com/sebanc/brunch/releases/download/r88-stable-20210223/brunch_r88_stable_20210223.tar.gz
+curl -# -Lo chromeos.zip https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_13505.73.0_guado_recovery_stable-channel_mp-v2.bin.zip
+# Download Brunch release r88
+#curl -# -Lo brunch.tar.gz https://github.com/sebanc/brunch/releases/download/r89-stable-20210403/brunch_r89_stable_20210403.tar.gz
+curl -# -Lo brunch.tar.gz https://github.com/sebanc/brunch/releases/download/r88-stable-20210223/brunch_r88_stable_20210223.tar.gz
 # Instructions
 echo "1. sudo apt install -y cgpt pv tar"
 echo "2. tar zxvf brunch.tar.gz"
