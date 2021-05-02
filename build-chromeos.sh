f2730788bfdccf158543b4d39059314c6fa5ed7a
rosie <askrichardrose@gmail.com>
2021-05-02 09:06:55 +0100

    Update build-chromeos.sh
    typo

--- build-chromeos.sh
+++ build-chromeos.sh
@@ -7,8 +7,8 @@ curl -Lo chromeos.zip https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos
 #curl -Lo brunch.tar.gz https://github.com/sebanc/brunch/releases/download/r89-stable-20210403/brunch_r89_stable_20210403.tar.gz
 curl -Lo brunch.tar.gz https://github.com/sebanc/brunch/releases/download/r88-stable-20210223/brunch_r88_stable_20210223.tar.gz
 # Instructions
-echo "1. sudo apt install cgpt pv tar"
+echo "1. sudo apt install -y cgpt pv tar"
 echo "2. tar zxvf brunch.tar.gz"
-echo "3. unzip chrome.zip"
+echo "3. unzip chromeos.zip"
 echo "4. lsblk"
 echo "5. sudo bash chromeos-install.sh -src CHROME.img > -dst /dev/sdX"
