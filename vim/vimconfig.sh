742c5596bc1746041f010c5198d8de8c5c2cc574
Rich Rose <askrichardrose@gmail.com>
2022-04-16 07:57:14 +0100

    Update vimconfig.sh
    Amend bash script to check for file/directory before performing update

--- vim/vimconfig.sh
+++ vim/vimconfig.sh
@@ -1,9 +1,17 @@
-# vim-plug installation
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#!/bin/sh
 
-# Create an undo directory
-mkdir -p ~/.vim/undodir
+if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
+  # vim-plug installation
+  curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/main/plug.vim
+fi
 
-# Download vim configuration
-curl -fLo ~/.vimrc https://raw.githubusercontent.com/rosera/machine-config/master/vim/.vimrc
+if [ ! -d "$HOME/.vim/undodir" ]; then
+  # Create an undo directory
+  mkdir -p $HOME/.vim/undodir
+fi
+
+if [ ! -f "$HOME/.vimrc" ]; then
+  # Download vim configuration
+  curl -fLo $HOME/.vimrc https://raw.githubusercontent.com/rosera/machine-config/main/vim/.vimrc
+fi
