a1380ae2751bd6bc1de95a31eb805e434fe30192
Rich Rose <askrichardrose@gmail.com>
2022-04-16 23:25:40 +0100

    Update install.sh
    Amend the directory + file

--- scripts/go/install.sh
+++ scripts/go/install.sh
@@ -2,11 +2,11 @@
 
 GOLANG_VERSION="1.15"
 
-if [ ! -f "/usr/local/bin/go" ]; then
+if [ ! -d "/usr/local/go/bin" ]; then 
   # Download the file
   curl -Lo /tmp/go${GOLANG_VERSION}.linux-amd64.tar.gz https://golang.org/dl/go${GOLANG_VERSION}.linux-amd64.tar.gz
 fi
 
-if [ ! -f "/tmp/go${GOLANG_VERSION}.linux-amd64.tar.gz" ]; then
+if [ -f "/tmp/go${GOLANG_VERSION}.linux-amd64.tar.gz" ]; then
   sudo tar -C /usr/local -xzf /tmp/go${GOLANG_VERSION}.linux-amd64.tar.gz
 fi
