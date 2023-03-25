4b9f0c657d5bdc9691f2e8c45cc8801409037d1b
Rich Rose <askrichardrose@gmail.com>
2023-03-25 19:18:27 +0000

    Update net.sh
    Fix: Invalid variable definition.

--- tmux/.tmux_scripts/net.sh
+++ tmux/.tmux_scripts/net.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PLATFORM=$(uname -o)
-if [ PLATFORM == "Darwin" ]; then
+if [ "$PLATFORM" == "Darwin" ]; then
   ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}'
 else
   ip -o addr show scope global | tr -s ' ' | tr '/' ' ' | cut -f 2,4 -d ' ' | grep eth0
