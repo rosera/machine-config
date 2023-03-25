cf8ff5528286b90df41752be3e6daa9f1f34635e
Rich Rose <askrichardrose@gmail.com>
2023-03-25 19:05:40 +0000

    Update net.sh
    Make script generic for Linux and MacOS

--- tmux/.tmux_scripts/net.sh
+++ tmux/.tmux_scripts/net.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-ip -o addr show scope global | tr -s ' ' | tr '/' ' ' | cut -f 2,4 -d ' ' | grep eth0
+PLATFORM=$(uname -o)
+if [ PLATFORM == "Linux" ]; then
+  ip -o addr show scope global | tr -s ' ' | tr '/' ' ' | cut -f 2,4 -d ' ' | grep eth0
+else
+  ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}'
+fi
